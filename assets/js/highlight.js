968db69ec3e139784ca28c322401b79c8142f5c6
Michael Sasser <Michael@MichaelSasser.org>
2022-03-16 17:35:54 +0100

    feat: move to full version for better customization

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,25 +1,21 @@
 import hljs from 'highlight.js/lib/core';
 
-// import javascript from 'highlight.js/lib/languages/javascript';
+import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
 import xml from 'highlight.js/lib/languages/xml';
-// import ini from 'highlight.js/lib/languages/ini';
+import ini from 'highlight.js/lib/languages/ini';
 import yaml from 'highlight.js/lib/languages/yaml';
 import markdown from 'highlight.js/lib/languages/markdown';
-import python from 'highlight.js/lib/languages/python';
-import powershell from 'highlight.js/lib/languages/powershell';
 
-// hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('html', xml);
-// hljs.registerLanguage('ini', ini);
-// hljs.registerLanguage('toml', ini);
+hljs.registerLanguage('ini', ini);
+hljs.registerLanguage('toml', ini);
 hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('md', markdown);
-hljs.registerLanguage('python', python);
-hljs.registerLanguage('powershell', powershell);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
